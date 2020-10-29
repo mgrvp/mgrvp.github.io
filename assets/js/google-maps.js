26902e81b15a095e027c828375faad80bbfe42c4
mgrvp <mohgp855@gmail.com>
2020-10-29 11:19:42 +0700

    maps

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,7 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -7.971364, lng: 112.614362};
+    var myLatLng = {lat: -7.874608105470263, lng: 112.00204051653574};
+    // -7.874608105470263, 112.00204051653574
 
     var map = new google.maps.Map(document.getElementById('myMap'), {
         zoom: 17,
